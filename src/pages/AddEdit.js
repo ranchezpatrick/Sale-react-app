0822d0ce2a7169bca0f38d965b137c400799de6a
ranchezpatrick <ranchezpatrick@gmail.com>
2022-03-29 12:51:37 +0800

    Completed form compnent for adding items

--- src/pages/AddEdit.js
+++ src/pages/AddEdit.js
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, {useState, useEffect}from 'react';
+import {useHistory, useParams} from "react-router-dom";
+import './AddEdit.css';
+import fireDb from "../firebase";
+import {toast} from "react-toastify";
 
+
+const initialState = {
+    name: "",
+    size: "",
+    cost: "",
+    category: "",
+
+}
 const AddEdit = () => {
+    const [state, setState] = useState(initialState);
+    const [data, setData] = useState({});
+
+    const {name, size, category, cost} = state;
+    const handleInputChange = () => {};
+    const handleSubmit = () => {};
     return (
-        <div>
-            <h2>Add Edit</h2>
+        <div style={{marginTop: "100px"}}>
+            <form style={{
+                margin: "auto", 
+                padding: "15px", 
+                maxWidth: "400px", 
+                alignContent: "center"
+                }}
+                onSubmit={handleSubmit}
+            >
+
+
+                <label htmlFor="name">
+                Name
+                </label>
+                <input 
+                type="text"
+                id= "name"
+                name= "name"
+                placeHolder= "Your Name..."
+                value={name}
+                onChange={handleInputChange}
+                />
+
+                <label htmlFor="cost">
+                Cost
+                </label>
+                <input 
+                type="number"
+                id= "size"
+                name= "size"
+                placeHolder= "Small, Medium or Large"
+                value={name}
+                onChange={handleInputChange}
+                />
+
+                <label htmlFor="size">
+                Size
+                </label>
+                <input 
+                type="text"
+                id= "cost"
+                name= "cost"
+                placeHolder= "Cost"
+                value={name}
+                onChange={handleInputChange}
+                />
+
+                <label htmlFor="category">
+                Category
+                </label>
+                <input 
+                type="text"
+                id= "category"
+                name= "category"
+                placeHolder= "Category"
+                value={name}
+                onChange={handleInputChange}
+                />  
+
+                <input type="submit" value="Save" />
+                </form>
         </div>
     )
 }
